890564ba505385b9cfaa683650bcc495d256f6b6
andlon93 <andreas.lones@gmail.com>
2015-03-11 10:43:06 +0100

    made a node class

--- Exercise4/exercise4.py
+++ Exercise4/exercise4.py
@@ -1,4 +1,11 @@
 #Exercise 4
+# Create tree: http://ironcreek.net/phpsyntaxtree/
+
+class node():
+	def __init__(self, data):
+		self.data = data
+		self.children = {}
+
 
 def read_from_file(name):
 	liste = []
