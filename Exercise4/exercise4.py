b0da7e41c75d849c50a4e6d9ef7c2fff6d51ac28
andlon93 <andreas.lones@gmail.com>
2015-03-17 12:09:26 +0100

    m

--- Exercise4/exercise4.py
+++ Exercise4/exercise4.py
@@ -1,6 +1,9 @@
 #Exercise 4
 # Create tree: http://ironcreek.net/phpsyntaxtree/
 
+import math as m
+import random as r
+
 class node():
 	def __init__(self, data):
 		self.data = data
@@ -21,6 +24,47 @@ def print_list(l):
 	for line in l:
 		print line
 
+def Plurality(examples):
+	ones_count = 0
+	twos_count = 0
+
+	for n in xrange(0, len(examples)):
+		if examples[-1] == '1': 
+			ones_count += 1
+		else: 
+			twos_count += 1
+
+	if ones_count > twos_count: 
+		return 1
+	elif twos_count > ones_count: 
+		return 2
+	else:
+		return r.randint(1,2)
+
+def same_classification(examples):
+	classification = examples[0][-1]
+	for n in xrange(1, len(examples)):
+		if n[-1] != classification:
+			return False
+	return True
+
+def B(q):
+	if q = 0: 
+		return q
+	else:
+		return -( ( q*m.log(q, 2) ) + ( (1.0-q)*m.log((1.0-q), 2) ) )
+
+def importance(data, attributes):
+	attribute_entropy = [None]*len(attributes)
+
+	for attribute in attributes:
+		count = 0
+		for liste in data:
+			if liste[attribute] == data[0][attribute]:
+				count += 1
+		attribute_entropy[attribute] = B(count/len(data))
+	return min(attribute_entropy)
+
 
 def decision_tree_learinng(examples, attributes, parent_examples):
 	if not examples:
@@ -30,7 +74,7 @@ def decision_tree_learinng(examples, attributes, parent_examples):
 	elif not attributes:
 		return Plurality(examples)
 	else:
-		A = importance(a, examples)
+		A = importance(examples, attributes)
 		tree = node(A)
 		attributes.remove(A)
 		for n in xrange(1,3):
@@ -50,8 +94,9 @@ def decision_tree_learinng(examples, attributes, parent_examples):
 def main():
 	test = read_from_file("test")
 	print_list(test)
-	print "\n"+"\n"
-	read_from_file("training")
+	training = read_from_file("training")
+	#print "\n"+"\n"
+	#print_list(training)
 
 
 main()
